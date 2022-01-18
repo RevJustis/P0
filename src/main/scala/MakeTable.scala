079cb02fd5e63c793985668554276f56c121b6f8
RevJustis <97845336+RevJustis@users.noreply.github.com>
2022-01-18 15:06:00 -0800

    Updates MakeTable.scala Minor updates

--- src/main/scala/MakeTable.scala
+++ src/main/scala/MakeTable.scala
@@ -4,18 +4,18 @@ import java.sql.SQLException
 import java.sql.Statement
 
 
-object MakeTable {
+class MakeTable {
   @throws[SQLException]
-  def main(args: Array[String]): Unit = { //Registering the Driver
+  def make(): Unit = { //Registering the Driver
     //DriverManager.registerDriver(new Nothing)
     //Getting the connection
     val mysqlUrl = "jdbc:mysql://localhost/mydatabase"
-    val con = DriverManager.getConnection(mysqlUrl, "root", "password")
+    val con = DriverManager.getConnection(mysqlUrl, "root", "rootpass")
     System.out.println("Connection established......")
     //Creating the Statement
     val stmt = con.createStatement
     //Creating the sales table with auto-incremented values
-    val createQuery = "CREATE TABLE `table`(ID INT PRIMARY KEY AUTO_INCREMENT, lane1 INT, lane2 INT, lane3 INT)"
+    val createQuery = "CREATE TABLE `table2`(ID INT PRIMARY KEY AUTO_INCREMENT, lane1 INT, lane2 INT, lane3 INT)"
     //Executing the query
     stmt.execute(createQuery)
     System.out.println("Table created......")
