929611c618df29f2d8db809d0a165271073186b6
RevJustis <97845336+RevJustis@users.noreply.github.com>
2022-01-18 12:19:27 -0800

    Modifies P0.scala

--- src/main/scala/P0.scala
+++ src/main/scala/P0.scala
@@ -1,5 +1,22 @@
-class P0 {
-  def main(args: Array[String]): Unit ={
+import scala.io.StdIn.readLine
 
+class P0 {
+  val trans: Array[String] = new Array[String]("reverseAll", "invertAll", "oddifyAll", "evenifyAll")
+
+  def main(args: Array[String]): Unit ={
+    //try {
+      val x: Char = readLine("So, think the lanes will favor you today?\nOr will you shuffle them? Press y or n: ")
+//    } catch {
+//    }
+    if (x == 'y') {
+      print("\n~~~~~~~~~~\n")
+      ShuffleTerm()
+    }
+  }
+
+  def ShuffleTerm(): Unit ={
+    println("Ah, the \"hands on\" type... Here are your options:")
+    //trans.foreach
+    val in = readLine("Ah, the \"hands on\" type... Here are your options:")
   }
 }
