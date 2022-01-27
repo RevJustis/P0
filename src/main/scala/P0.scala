bc5bf15a6bc1c3e59ed050d75e485ecdb8b3129f
RevJustis <97845336+RevJustis@users.noreply.github.com>
2022-01-26 23:03:30 -0800

    Modifies number of records to 200

--- src/main/scala/P0.scala
+++ src/main/scala/P0.scala
@@ -1,6 +1,7 @@
-import java.sql.{Connection, DriverManager}
-import Utilities._
 import CRUD._
+import Utilities._
+
+import java.sql.{Connection, DriverManager}
 
 object P0 {
   private val trans: Array[String] = Array("Reverse Tracks", "No Odd Debuffs", "No Even Debuffs")
@@ -45,13 +46,13 @@ object P0 {
 
   def play(con: Connection, track: Byte): Int = {
     var i: Byte = 1// Iterator counter
-    var result: Int = 0
+    var result = 0
     while (i < track + 1) {
       println("_ROUND " + i + " START_")
       println("There are three lanes in front of you, each with 200 collectibles of differing " +
         "values, plus buffs and debuffs!")
       println("Please choose a lane, and we will see how you fare...\n~1~\n~2~\n~3~")
-      var lane: Byte = choose123()// Gets users lane choice as a Byte
+      var lane = choose123()// Gets users lane choice as a Byte
       // Runs the read function, which returns the score of the full run on user's chosen track and lane
       result += read(con, i, lane)
       i = (i + 1).toByte// Increment so loop will eventually terminate
